22df35a6e846c963ec1a6993da8974afa0cc016d
Zozulia <Zozulia@908.dp.ua>
2025-08-04 17:48:54 +0300

    currencies in sidebar

--- components/UI/Sidebar.vue
+++ components/UI/Sidebar.vue
@@ -110,6 +110,31 @@
               Категории
             </NuxtLink>
           </li>
+          <li>
+            <NuxtLink
+              to="/currencies"
+              class="flex items-center px-3 py-2 rounded-xl text-gray-700 hover:bg-blue-100 transition font-medium"
+              active-class="bg-blue-50 text-blue-700"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="w-5 h-5 mr-2"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                aria-hidden="true"
+                role="img"
+              >
+                <path
+                  d="M12 1v22M17 5H9.5a3.5 3.5 0 1 0 0 7H14a3.5 3.5 0 1 1 0 7H6"
+                />
+              </svg>
+              <span>Валюта</span>
+            </NuxtLink>
+          </li>
         </ul>
       </nav>
     </aside>
